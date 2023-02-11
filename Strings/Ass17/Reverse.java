c16f9e8bd14460a312ff29554953b5d68874ddce
Blackwatch1910 <toretto_dominic1970@hotmail.com>
2023-02-12 01:55:12 +0530

    Reverse whole string

---
+++ Strings/Ass17/Reverse.java
@@ -0,0 +1,22 @@
+class Reverse {
+    static String reverse(String s) {
+        String str = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            str += s.charAt(i);
+        }
+        return str;
+    }
+
+    public static void main(String a[]) {
+        String s = "devanshu yadav";
+        String sArr[] = s.split(" ");
+        String reversedString = "";
+    
+        String newReversedString = "";
+        for (int i = sArr.length - 1; i >= 0; i--) {
+            newReversedString += reverse(sArr[i]) + " ";
+        }
+
+        System.out.println(newReversedString);
+    }
+}
